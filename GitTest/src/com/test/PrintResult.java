32e3f0ffe1d9ca1a4a0dd491b01f34ca26646bfd
baljeet <malik@consultdaffodil.com>
2016-01-09 21:07:18 +0530

    Initial checking

---
+++ GitTest/src/com/test/PrintResult.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class PrintResult {
+
+	public static void main(String args[]){
+		System.out.println(Calculator.add(5, 10));
+		System.out.println(Calculator.subtract(10, 5));
+	}
+}
